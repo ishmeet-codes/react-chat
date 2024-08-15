d36342afdd686ce6aa4bafb053c507ccb041a8f6
ishmeet-codes <ishmeetsinghxa@gmail.com>
2024-08-15 16:58:44 +0530

    updated

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,8 +1,7 @@
-
-import React from "react";
+import React , { useEffect, useRef, useState } from "react";
 import Message from "./Message";
 import SendMessage from "./SendMessage";
-import React, { useEffect, useRef, useState } from "react";
+
 const scroll = useRef();
 
 const ChatBox = () => {
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { auth } from "../firebase";
+import { auth } from "src/firebase.js";
 import { useAuthState } from "react-firebase-hooks/auth";
 const Message = ({ message }) => {
   const [user] = useAuthState(auth);
